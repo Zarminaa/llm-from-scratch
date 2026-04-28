b7a7fa8707306c29ead75ad0da7386b42cc6dce9
Zarminaa <zarrminaa@gmail.com>
2026-04-28 22:27:58 +0500

    made token class v2 (just added <|unk|> and <|endoftext|>

--- tokenizer.py
+++ tokenizer.py
@@ -31,3 +31,42 @@ class SimpleTokenizerV1:
         text=" ".join(self.int_to_str[i] for i in ids)
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
+
+# tokenizer = SimpleTokenizerV1(vocab)
+# text = """"It's the last he painted, you know," 
+#            Mrs. Gisburn said with pardonable pride."""
+# ids = tokenizer.encode(text)
+# print(ids)
+
+all_tokens=sorted(set(preprocessed))
+all_tokens.extend(["<|endoftext|>","<|unk|>"])
+vocab={token:integer for integer,token in enumerate(all_tokens)}
+# print(len(vocab.items()))
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int
+            else "<|unk|>" for item in preprocessed
+        ] # we have this change.
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+    
+# tokenizer = SimpleTokenizerV2(vocab)
+# text1 = "Hello, do you like tea?"
+# text2 = "In the sunlit terraces of the palace."
+# text = " <|endoftext|> ".join((text1, text2))
+# print(text)
