1e69e96f7e09efdd7ad3975bbca52ce7308cb730
Zarminaa <zarrminaa@gmail.com>
2026-05-03 01:29:00 +0500

    saw simplified self attention using a small example and saw hhow we calculate context vector for a token embedding

---
+++ attention.py
@@ -0,0 +1,50 @@
+#IMPLEMENTING A SIMPLIFIED ATTENTION MECHANISM
+import torch
+
+inputs=torch.tensor(
+[
+   [0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55] # step     (x^6)
+]
+)
+
+query = inputs[1]  # 2nd input token is the query
+attn_scores_2 = torch.empty(inputs.shape[0])
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query) # dot product (transpose not necessary here since they are 1-dim vectors)
+
+print(attn_scores_2)
+
+#normailzing
+attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum() # attention score  / sum of all scores
+
+print("Attention weights:", attn_weights_2_tmp) 
+print("Sum:", attn_weights_2_tmp.sum())
+
+#better way to do normalization (softmax function)
+
+def softmax_naive(x):
+    return torch.exp(x) / torch.exp(x).sum(dim=0)
+
+attn_weights_2_naive = softmax_naive(attn_scores_2)
+
+print("Attention weights:", attn_weights_2_naive)
+print("Sum:", attn_weights_2_naive.sum())
+
+#pytorch implementation of softmax
+attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
+print("Attention weights:", attn_weights_2)
+print("Sum:", attn_weights_2.sum())
+
+#context vector calculation
+query = inputs[1] # 2nd input token is the query
+
+context_vec_2 = torch.zeros(query.shape)
+for i,x_i in enumerate(inputs):
+    context_vec_2 += attn_weights_2[i]*x_i
+
+print(context_vec_2)
