00250dbcc3edae50e5aba812c76c719f2952a814
Zarminaa <zarrminaa@gmail.com>
2026-05-06 03:42:50 +0500

    Looked at cross entropy and preplexity score

---
+++ loss.py
@@ -0,0 +1,51 @@
+
+import torch
+from gpt import GPTModel, GPT_CONFIG_124M
+
+inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107,  588, 11311]]) #  " really like chocolate"]
+
+model = GPTModel(GPT_CONFIG_124M)
+
+with torch.no_grad():
+    logits = model(inputs)
+
+probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+print(probas.shape) # Shape: (batch_size, num_tokens, vocab_size)
+
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+# Compute logarithm of all token probabilities
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+# Calculate the average probability for each token
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
+
+neg_avg_log_probas = avg_log_probas * -1
+print(neg_avg_log_probas)
+
+
+#shortcut
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+
+print("Flattened logits:", logits_flat.shape)
+print("Flattened targets:", targets_flat.shape)
+
+loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+print(loss)
+
+perplexity = torch.exp(loss)
+print(perplexity)
